d9c40d6ba66e996905424eba27807b3b991bf59e
kadirgurturk <89667179+kadirgurturk@users.noreply.github.com>
2023-07-10 23:47:49 +0300

    added new query

--- QuizApp/src/main/java/com/kadirgurturk/QuizApp/database/LikeRepository.java
+++ QuizApp/src/main/java/com/kadirgurturk/QuizApp/database/LikeRepository.java
@@ -2,9 +2,11 @@ package com.kadirgurturk.QuizApp.database;
 
 import com.kadirgurturk.QuizApp.entity.Like;
 import com.kadirgurturk.QuizApp.entity.User;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,8 +18,10 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
 
     List<Like> findByPostId(Long postId);
 
+    @Transactional
     @Modifying
     @Query("DELETE FROM Like l WHERE l.post.id = :postId AND l.user.id = :userId")
-    void deleteByPostIdAndUserId(Long postId, Long userId);
+    int deleteByPostIdAndUserId(@Param("postId") Long postId, @Param("userId") Long userId);
+
 
 }
